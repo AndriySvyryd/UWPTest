c4513593bea5757db3024ea572b8339daad7cb96
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2017-07-31 17:03:47 -0700

    Add a test app

---
+++ Verification/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Verification
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RunTests_Click(object sender, RoutedEventArgs e)
+        {
+            RunTestsButton.IsEnabled = false;
+            OutputTextBlock.Text = "";
+            Task.Run(RunTests);
+        }
+
+        private async Task RunTests()
+        {
+            await WriteLine("Running tests...");
+
+            var testContainer = new Tests(WriteLine);
+
+            foreach (var testMethod in testContainer.GetType().GetTypeInfo().GetRuntimeMethods()
+                .Where(m => m.ReturnType == typeof(Task)))
+            {
+                try
+                {
+                    await (Task)testMethod.Invoke(testContainer, new object[0]);
+                }
+                catch (Exception ex)
+                {
+                    await WriteLine($"Test {testMethod.Name} failed:");
+                    await WriteLine(ex.ToString());
+                    await WriteLine("");
+                }
+            }
+
+            await WriteLine("Done");
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+                () => { RunTestsButton.IsEnabled = true; }).AsTask();
+        }
+
+        private Task WriteLine(string text)
+        {
+            return Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => { OutputTextBlock.Text += text + Environment.NewLine; }).AsTask();
+        }
+    }
+}
---
+++ Verification/Tests.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Verification
+{
+    public class Tests
+    {
+        protected Func<string, Task> WriteLine { get; }
+
+        public Tests(Func<string, Task> output)
+        {
+            WriteLine = output;
+        }
+
+        public async Task CompiledQuery()
+        {
+            using (var context = new VerificationApplicationContext())
+            {
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.EnsureCreatedAsync();
+
+                var blog = context.CreateBlog();
+                blog.Name = "Hello World!";
+
+                context.CreatePost(blog).Title = "First";
+
+                var blog2 = context.CreateBlog();
+                blog2.Name = "0x000 is the new black";
+
+                var post2 = context.CreatePost(blog2);
+                post2.Title = "Black is back";
+
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new VerificationApplicationContext())
+            {
+                var query = EF.CompileQuery((VerificationApplicationContext c, string t)
+                    => c.Set<Post>().Include(p => p.Blog).Where(p => p.TenantId == t));
+                Assert.Equal(2, context.Blogs.Count());
+                Assert.Equal(0, context.Set<Blog>().Local.Count());
+                var posts = query(context, "1").ToList();
+                Assert.Equal(2, posts.Count());
+                Assert.Equal(2, context.Set<Post>().Local.Count());
+            }
+        }
+
+        public async Task AnonymousTypeProjection()
+        {
+            using (var context = new VerificationApplicationContext())
+            {
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.EnsureCreatedAsync();
+
+                var blog = context.CreateBlog();
+                blog.Name = "Hello World!";
+
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new VerificationApplicationContext())
+            {
+                var result = (from b in context.Blogs.Select(b => new { b })
+                              from l in context.Blogs.Select(l => new { l })
+                              where b.b.Id == l.l.Id
+                              select new { b, l }).First();
+
+                Assert.Same(result.b.b, result.l.l);
+            }
+        }
+
+        protected class VerificationApplicationContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+            public DbSet<Tenant> Tenants { get; set; }
+
+            public VerificationApplicationContext()
+                : base()
+            {
+            }
+
+            public VerificationApplicationContext(DbContextOptions<VerificationApplicationContext> options)
+                : base(options)
+            {
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                base.OnConfiguring(optionsBuilder);
+
+                optionsBuilder.UseSqlite("Data Source=Verification.db")
+                    .EnableSensitiveDataLogging()
+                    .ConfigureWarnings(w => w.Default(WarningBehavior.Throw).Ignore(CoreEventId.SensitiveDataLoggingEnabledWarning));
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Blog>(bb => { });
+
+                modelBuilder.Entity<Post>(pb =>
+                {
+                    pb.HasOne(p => p.Blog).WithMany(b => b.Posts).OnDelete(DeleteBehavior.ClientSetNull);
+                });
+
+                modelBuilder.Entity<Post>();
+            }
+
+            public Blog CreateBlog()
+            {
+                var currentTenant = Tenants.Find("1");
+                if (currentTenant == null)
+                {
+                    Tenants.Add(new Tenant { Id = "1" });
+                }
+                var blog = new Blog { TenantId = "1" };
+                Blogs.Add(blog);
+                return blog;
+            }
+
+            public Post CreatePost(Blog blog)
+            {
+                var post = new Post { Blog = blog, TenantId = blog.TenantId };
+                Posts.Add(post);
+                return post;
+            }
+        }
+
+        protected class Blog
+        {
+            public string TenantId { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public ICollection<Post> Posts { get; set; }
+            public Tenant Tenant { get; set; }
+        }
+
+        protected class Post
+        {
+            public string TenantId { get; set; }
+            public int BlogId { get; set; }
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public Blog Blog { get; set; }
+            public Tenant Tenant { get; set; }
+        }
+
+        protected class Tenant
+        {
+            public string Id { get; set; }
+        }
+    }
+}
